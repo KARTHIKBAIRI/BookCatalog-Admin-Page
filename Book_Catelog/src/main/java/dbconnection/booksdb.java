6067da220430afa34bf3864b7b5e9fba8ac293ce
Karthik Bairi <133215834+KARTHIKBAIRI@users.noreply.github.com>
2025-03-19 23:33:31 +0530

    Update booksdb.java

--- Book_Catelog/src/main/java/dbconnection/booksdb.java
+++ Book_Catelog/src/main/java/dbconnection/booksdb.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class booksdb {
 	private static String Driver = "com.mysql.cj.jdbc.Driver";
 	private static String user = "root";
-	private static String password = "Karthikb@11";
+	private static String password = "Karthik";
 	private static String url = "jdbc:mysql://localhost:3306/bookcat";
 	private static Connection conn = null;
 	public static Connection conn() {
